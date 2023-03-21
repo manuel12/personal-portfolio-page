1363c82c7474762e0f7423b0d75d28312a92198a
Manuel <manuelpinedacabeza@gmail.com>
2023-03-21 17:14:59 +0100

    Updated navbar to use MUI elements.

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+import MenuIcon from "@mui/icons-material/Menu";
+
 import "./styles.css";
 
 const Navbar = () => {
+  const pages = ["about", "skills", "projects", "contact"];
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    console.log(event.currentTarget);
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <>
-      <nav class="navbar" id="navbar">
-        <button class="hamburger-btn" id="hamburger-btn">
-          <span class="bar"></span>
-          <span class="bar"></span>
-          <span class="bar"></span>
-        </button>
-        <ul class="navbar-list">
-          <a class="navbar-link" href="#welcome-section">
-            ABOUT
-          </a>
-          <a class="navbar-link" href="#skills">
-            SKILLS
-          </a>
-          <a class="navbar-link" href="#projects">
-            PROJECTS
-          </a>
-          <a class="navbar-link" href="#contact">
-            CONTACT
-          </a>
-        </ul>
-      </nav>
+      <AppBar className="navbar">
+        <Container maxWidth="false">
+          <Toolbar className="toolbar" disableGutters>
+            <Box
+              sx={{
+                display: {
+                  xs: "flex",
+                  md: "none",
+                },
+              }}
+            >
+              <IconButton size="small" onClick={handleOpenNavMenu}>
+                <MenuIcon sx={{ fontSize: 36 }} className="hamburger-button" />
+              </IconButton>
+
+              <Menu
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <a href={`#${page}`}>
+                    <MenuItem
+                      key={page}
+                      onClick={handleCloseNavMenu}
+                      className="navbar-menu-item"
+                      dense
+                    >
+                      <Typography textalign="left" variant="h5">
+                        {page}
+                      </Typography>
+                    </MenuItem>
+                  </a>
+                ))}
+              </Menu>
+            </Box>
+            <Box
+              className="navbar-buttons-container"
+              sx={{
+                display: {
+                  xs: "none",
+                  md: "inherit",
+                },
+              }}
+            >
+              {pages.map((page) => {
+                return (
+                  <a href={`#${page}`} key={page} className="navbar-link">
+                    <Button className="navbar-button" variant="outlined">
+                      {page}
+                    </Button>
+                  </a>
+                );
+              })}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </>
   );
 };
