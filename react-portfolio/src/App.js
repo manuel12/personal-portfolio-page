e8649cf9601c9b6bd79f59a0385bd294f31e06fe
Manuel <manuelpinedacabeza@gmail.com>
2023-03-21 12:44:06 +0100

    Imported contact component

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
