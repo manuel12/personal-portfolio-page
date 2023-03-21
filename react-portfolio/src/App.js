f81fc01025e50a1e916c088fd3df39a920184685
Manuel <manuelpinedacabeza@gmail.com>
2023-03-21 12:25:22 +0100

    Imported and added navbar, skills and projects sections to the app.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import Skills from "./components/Skills/Skills";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
+      <Skills />
+      <Projects />
     </div>
   );
 }
