1cf2e9de52ba55a54d9f3564e315cb70ee4aa2b5
Manuel <manuelpinedacabeza@gmail.com>
2023-03-22 10:30:17 +0100

    Imported and added animations components, states and functions.

--- react-portfolio/src/components/Home/Home.jsx
+++ react-portfolio/src/components/Home/Home.jsx
@@ -1,30 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Zoom from "@mui/material/Zoom";
+
+import { createObserver, observeElements } from "../../helpers/helpers";
 import "./styles.css";
 
 const Home = () => {
+  const [headingAnimation, setHeadingAnimation] = useState(false);
+  const [textAnimation, setTextAnimation] = useState(false);
+
+  const animationFunctions = {
+    heading: setHeadingAnimation,
+    text: setTextAnimation,
+  };
+
+  const observer = createObserver(animationFunctions);
+
+  useEffect(() => {
+    observeElements(".about-animation", observer);
+  });
+
   return (
     <>
-      <section class="welcome-section" id="welcome-section">
-        <div class="about-container">
-          <h1 class="about-heading">
-            Manuel Pineda
-            <br />
-            Front-end, <br class="about-line-break" />
-            Back-end & Test Developer
-          </h1>
-
-          <p class="about-text">
-            Hi! I'm Manuel. How are you doing? I love all sides of programing
-            web apps, from the backend, to frontend and testing.
-            <br />
-            I've done each. I believe in being able to build all parts of an
-            application in order to fully understand how it works.
-            <br />
-            <br />
-            In my free time I enjoy reading, running and photography.
-          </p>
-        </div>
-      </section>
+      <Box class="welcome-section" id="about">
+        <Container class="about-container">
+          <Zoom in={headingAnimation} timeout={1200}>
+            <Typography
+              textAlign="center"
+              variant="h3"
+              id="heading"
+              className="about-heading about-animation"
+            >
+              Manuel Pineda
+              <br />
+              Front-end, <br class="about-line-break" />
+              Back-end & Test Developer
+            </Typography>
+          </Zoom>
+          <Zoom in={textAnimation} timeout={1600}>
+            <Typography
+              variant="h6"
+              id="text"
+              className="about-text about-animation"
+            >
+              Hi! I'm Manuel. How are you doing? I love all sides of programing
+              web apps, from the backend, to frontend and testing.
+              <br />
+              I've done each. I believe in being able to build all parts of an
+              application in order to fully understand how it works.
+              <br />
+              <br />
+              In my free time I enjoy reading, running and photography.
+            </Typography>
+          </Zoom>
+        </Container>
+      </Box>
     </>
   );
 };
