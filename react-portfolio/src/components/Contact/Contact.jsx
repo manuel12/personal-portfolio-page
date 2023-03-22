185950d3f17d8ce94a96cb01dc8a41fbbb219e26
Manuel <manuelpinedacabeza@gmail.com>
2023-03-22 18:52:47 +0100

    Added contact-card. Added all icons, text, inputs and buttons inside. Updated animations.

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -36,88 +36,97 @@ const Contact = () => {
   return (
     <Box class="contact" id="contact">
       <Container class="contact-container">
-        <Zoom in={contactHeadingAnimation} timeout={1200}>
-          <Typography
-            variant="h3"
-            id="contactHeading"
-            className="contact-heading contact-animation"
-          >
-            Contact
-          </Typography>
-        </Zoom>
+        <div className="contact-card">
+          <Zoom in={contactHeadingAnimation} timeout={1200}>
+            <Typography
+              variant="h3"
+              id="contactHeading"
+              className="contact-heading contact-animation"
+            >
+              Contact
+            </Typography>
+          </Zoom>
 
-        <div class="icon-section">
-          <Zoom in={socialIconsAnimation} timeout={2000}>
-            <a
-              href="mailto:manuelpinedacabeza.com"
-              target="_blank"
-              id="socialIcon"
-              className="contact-animation"
-            >
-              <div class="icon-container">
-                <EmailIcon />
-              </div>
-            </a>
-          </Zoom>
-          <Zoom in={socialIconsAnimation} timeout={2500}>
-            <a
-              href="https://www.linkedin.com/"
-              target="_blank"
-              id="socialIcon"
-              className="contact-animation"
-            >
-              <div class="icon-container">
-                <LinkedInIcon />
-              </div>
-            </a>
-          </Zoom>
-          <Zoom in={socialIconsAnimation} timeout={3000}>
-            <a
-              href="https://github.com/manuel12"
-              target="_blank"
-              id="socialIcon"
-              className="contact-animation"
-            >
-              <div class="icon-container">
-                <GitHubIcon />
-              </div>
-            </a>
-          </Zoom>
-        </div>
-        <Slide direction="right" in={contactQuestionAnimation} timeout={1500}>
-          <Typography
-            variant="body1"
-            id="contactQuestion"
-            className="contact-heading contact-animation"
-          >
-            Do you want to work together?
-          </Typography>
-        </Slide>
+          <div class="icon-section">
+            <Zoom in={socialIconsAnimation} timeout={2000}>
+              <a
+                href="mailto:manuelpinedacabeza.com"
+                target="_blank"
+                id="socialIcon"
+                className="contact-animation"
+              >
+                <div class="icon-container">
+                  <EmailIcon />
+                </div>
+              </a>
+            </Zoom>
+            <Zoom in={socialIconsAnimation} timeout={2500}>
+              <a
+                href="https://www.linkedin.com/"
+                target="_blank"
+                id="socialIcon"
+                className="contact-animation"
+              >
+                <div class="icon-container">
+                  <LinkedInIcon />
+                </div>
+              </a>
+            </Zoom>
+            <Zoom in={socialIconsAnimation} timeout={3000}>
+              <a
+                href="https://github.com/manuel12"
+                target="_blank"
+                id="socialIcon"
+                className="contact-animation"
+              >
+                <div class="icon-container">
+                  <GitHubIcon />
+                </div>
+              </a>
+            </Zoom>
+          </div>
 
-        <Slide direction="up" in={contactFormAnimation} timeout={1500}>
+          <Zoom in={contactQuestionAnimation} timeout={1500}>
+            <Typography
+              variant="body1"
+              id="contactQuestion"
+              className="contact-heading contact-animation"
+            >
+              Do you want to work together?
+            </Typography>
+          </Zoom>
+
           <Box component="form" id="contactForm" className="contact-animation">
-            <input
-              id="name"
-              type="text"
-              name="name"
-              placeholder="Your name..."
-            />
-            <input
-              id="email"
-              type="email"
-              name="email"
-              placeholder="Your email..."
-            />
-            <textarea
-              id="message"
-              name="message"
-              placeholder="Your message..."
-            ></textarea>
-            <button type="submit" class="submit-btn">
-              Submit
-            </button>
+            <Zoom in={contactFormAnimation} timeout={1800}>
+              <input
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Your name..."
+              />
+            </Zoom>
+            <Zoom in={contactFormAnimation} timeout={2200}>
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Your email..."
+              />
+            </Zoom>
+            <Zoom in={contactFormAnimation} timeout={2600}>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Your message..."
+              ></textarea>
+            </Zoom>
+            <Zoom in={contactFormAnimation} timeout={3000}>
+              <button type="submit" class="submit-btn">
+                Submit
+              </button>
+            </Zoom>
           </Box>
-        </Slide>
+        </div>
       </Container>
     </Box>
   );
