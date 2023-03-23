2fca3f2b0cc62ab47754dfba3a98a23c9fc5447f
Manuel <manuelpinedacabeza@gmail.com>
2023-03-23 17:24:59 +0100

    Added project data

---
+++ react-portfolio/src/data/projects.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    projectUrl: "https://github.com/manuel12/django-expense-tracker-public",
+    projectName: "Expense Tracker",
+    projectImgSrc: "images/expense-tracker.png",
+  },
+  {
+    projectUrl: "https://github.com/manuel12/motivation-vault",
+    projectName: "Motivation Vault",
+    projectImgSrc: "images/motivation-vault.png",
+  },
+  {
+    projectUrl: "https://github.com/manuel12/media-ratings",
+    projectName: "Media Ratings",
+    projectImgSrc: "images/media-ratings.png",
+  },
+
+  {
+    projectUrl: "https://github.com/manuel12/custom-css-library",
+    projectName: "Custom Library",
+    projectImgSrc: "images/custom-library.png",
+  },
+  {
+    projectUrl: "https://github.com/manuel12/technical-documentation-page",
+    projectName: "Custom Library - Documentation",
+    projectImgSrc: "images/technical-documentation-page.png",
+  },
+  {
+    projectUrl: "https://github.com/manuel12/landing-page",
+    projectName: "Landing Page",
+    projectImgSrc: "images/landing-page.png",
+  },
+
+  {
+    projectUrl: "https://github.com/manuel12/quote-project",
+    projectName: "Random Quote Generator",
+    projectImgSrc: "images/random-quote-generator.png",
+  },
+  {
+    projectUrl: "https://github.com/manuel12/25-plus-5-clock",
+    projectName: "25 + 5 Clock",
+    projectImgSrc: "images/25_plus_5_clock.png",
+  },
+  {
+    projectUrl: "https://github.com/manuel12/js-calculator",
+    projectName: "JS Calculator",
+    projectImgSrc: "images/js-calculator.png",
+  },
+];
