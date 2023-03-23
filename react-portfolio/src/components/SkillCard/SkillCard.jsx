160f9387c4ab86c153d0dd8258aa7923e7adee4f
Manuel <manuelpinedacabeza@gmail.com>
2023-03-23 17:24:45 +0100

    Added SkillCard component.

---
+++ react-portfolio/src/components/SkillCard/SkillCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Zoom from "@mui/material/Zoom";
+
+import "./styles.css";
+import "./darkStyles.css";
+
+const SkillCard = ({
+  iconElement,
+  headingText,
+  animationFunction,
+  animationTimeout,
+  children,
+}) => {
+  return (
+    <div className="skill-card">
+      <div className="skill-icon-section">
+        <Zoom in={animationFunction} timeout={animationTimeout}>
+          <div id="skillIcon" className="skill-icon-container skills-animation">
+            {iconElement}
+          </div>
+        </Zoom>
+      </div>
+      <div className="skill-text-section">
+        <Zoom in={animationFunction} timeout={animationTimeout}>
+          <Typography
+            id="skillIcon"
+            className="skill-text-section-heading skills-animation"
+            variant="h4"
+          >
+            {headingText}
+          </Typography>
+        </Zoom>
+        <Typography className="skill-text-section-content">
+          {children}
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default SkillCard;
