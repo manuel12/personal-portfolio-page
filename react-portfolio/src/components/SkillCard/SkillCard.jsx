da7d6596497a831630e2d90498d0f845f1823346
Manuel <manuelpinedacabeza@gmail.com>
2023-03-29 20:33:18 +0200

    Removed darkStyles file import

--- react-portfolio/src/components/SkillCard/SkillCard.jsx
+++ react-portfolio/src/components/SkillCard/SkillCard.jsx
@@ -3,7 +3,6 @@ import Typography from "@mui/material/Typography";
 import Zoom from "@mui/material/Zoom";
 
 import "./styles.css";
-import "./darkStyles.css";
 
 const SkillCard = ({
   iconElement,
