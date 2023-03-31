d54a25a06507a2e8905c87ce1f9d6e0d6bcf55cb
Manuel <manuelpinedacabeza@gmail.com>
2023-03-31 12:53:44 +0200

    Removed heading animation, heading animation states, and animation functions.
    Added zoom animation to contact card.

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -13,14 +13,14 @@ import { createObserver, observeElements } from "../../helpers/helpers";
 import "./styles.css";
 
 const Contact = () => {
-  const [contactHeadingAnimation, setContactHeadingAnimation] = useState(false);
+  const [contactCardAnimation, setContactCardAnimation] = useState(false);
   const [socialIconsAnimation, setSocialIconsAnimation] = useState(false);
   const [contactQuestionAnimation, setContactQuestionAnimation] =
     useState(false);
   const [contactFormAnimation, setContactFormAnimation] = useState(false);
 
   const animationFunctions = {
-    contactHeading: setContactHeadingAnimation,
+    contactCard: setContactCardAnimation,
     socialIcon: setSocialIconsAnimation,
     contactForm: setContactFormAnimation,
     contactQuestion: setContactQuestionAnimation,
@@ -34,97 +34,101 @@ const Contact = () => {
 
   return (
     <Box className="contact" id="contact">
-      <Container className="contact-container" maxWidth="lg">
-        <Zoom in={contactHeadingAnimation} timeout={1200}>
-          <Typography
-            variant="h3"
-            id="contactHeading"
-            className="contact-heading contact-animation"
-          >
-            Contact
-          </Typography>
-        </Zoom>
-        <div className="contact-card">
-          <div class="icon-section">
-            <Zoom in={socialIconsAnimation} timeout={2000}>
-              <a
-                href="mailto:manuelpinedacabeza.com"
-                target="_blank"
-                id="socialIcon"
-                className="contact-animation"
-              >
-                <div class="icon-container">
-                  <EmailIcon />
-                </div>
-              </a>
-            </Zoom>
-            <Zoom in={socialIconsAnimation} timeout={2500}>
-              <a
-                href="https://www.linkedin.com/"
-                target="_blank"
-                id="socialIcon"
-                className="contact-animation"
-              >
-                <div class="icon-container">
-                  <LinkedInIcon />
-                </div>
-              </a>
-            </Zoom>
-            <Zoom in={socialIconsAnimation} timeout={3000}>
-              <a
-                href="https://github.com/manuel12"
-                target="_blank"
-                id="socialIcon"
-                className="contact-animation"
-              >
-                <div class="icon-container">
-                  <GitHubIcon />
-                </div>
-              </a>
-            </Zoom>
-          </div>
+      <Container className="contact-container">
+        <Typography
+          variant="h3"
+          id="contactHeading"
+          className="contact-heading"
+        >
+          Contact
+        </Typography>
+        <Zoom in={contactCardAnimation} timeout={1000}>
+          <div id="contactCard" className="contact-card contact-animation">
+            <div class="icon-section">
+              <Zoom in={socialIconsAnimation} timeout={2000}>
+                <a
+                  href="mailto:manuelpinedacabeza.com"
+                  target="_blank"
+                  id="socialIcon"
+                  className="contact-animation"
+                >
+                  <div class="icon-container">
+                    <EmailIcon />
+                  </div>
+                </a>
+              </Zoom>
+              <Zoom in={socialIconsAnimation} timeout={2500}>
+                <a
+                  href="https://www.linkedin.com/"
+                  target="_blank"
+                  id="socialIcon"
+                  className="contact-animation"
+                >
+                  <div class="icon-container">
+                    <LinkedInIcon />
+                  </div>
+                </a>
+              </Zoom>
+              <Zoom in={socialIconsAnimation} timeout={3000}>
+                <a
+                  href="https://github.com/manuel12"
+                  target="_blank"
+                  id="socialIcon"
+                  className="contact-animation"
+                >
+                  <div class="icon-container">
+                    <GitHubIcon />
+                  </div>
+                </a>
+              </Zoom>
+            </div>
 
-          <Zoom in={contactQuestionAnimation} timeout={1500}>
-            <Typography
-              variant="body1"
-              id="contactQuestion"
+            <Zoom in={contactQuestionAnimation} timeout={1500}>
+              <Typography
+                variant="body1"
+                id="contactQuestion"
+                className="contact-animation"
+              >
+                Do you want to work together?
+              </Typography>
+            </Zoom>
+
+            <Box
+              component="form"
+              id="contactForm"
               className="contact-animation"
             >
-              Do you want to work together?
-            </Typography>
-          </Zoom>
-
-          <Box component="form" id="contactForm" className="contact-animation">
-            <Zoom in={contactFormAnimation} timeout={1800}>
-              <input
-                id="name"
-                type="text"
-                name="name"
-                placeholder="Your name..."
-              />
-            </Zoom>
-            <Zoom in={contactFormAnimation} timeout={2200}>
-              <input
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Your email..."
-              />
-            </Zoom>
-            <Zoom in={contactFormAnimation} timeout={2600}>
-              <textarea
-                id="message"
-                name="message"
-                placeholder="Your message..."
-              ></textarea>
-            </Zoom>
-            <Zoom in={contactFormAnimation} timeout={3000}>
-              <button type="submit" class="submit-btn">
-                Submit
-              </button>
-            </Zoom>
-          </Box>
-        </div>
+              <Zoom in={contactFormAnimation} timeout={1800}>
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  placeholder="Your name..."
+                />
+              </Zoom>
+              <Zoom in={contactFormAnimation} timeout={2200}>
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Your email..."
+                />
+              </Zoom>
+              <Zoom in={contactFormAnimation} timeout={2600}>
+                <textarea
+                  id="message"
+                  name="message"
+                  placeholder="Your message..."
+                ></textarea>
+              </Zoom>
+              <Zoom in={contactFormAnimation} timeout={3000}>
+                <button type="submit" class="submit-btn">
+                  Submit
+                </button>
+              </Zoom>
+            </Box>
+          </div>
+        </Zoom>
       </Container>
     </Box>
   );
