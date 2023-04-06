37d51234f0194c652de5b2bf048e60a05a9dddd1
Manuel <manuelpinedacabeza@gmail.com>
2023-04-06 14:38:33 +0200

    Replaced skillCardAnimations calls with boolean true.

--- react-portfolio/src/components/Skills/Skills.jsx
+++ react-portfolio/src/components/Skills/Skills.jsx
@@ -36,7 +36,7 @@ const Skills = () => {
         </Typography>
 
         <div className="skill-card-container">
-          <Zoom in={skillCardAnimation} timeout={400}>
+          <Zoom in={true} timeout={400}>
             <div
               id="skillCard"
               className="skill-card-animation-wrapper skills-animation"
@@ -61,7 +61,7 @@ const Skills = () => {
             </div>
           </Zoom>
 
-          <Zoom in={skillCardAnimation} timeout={600}>
+          <Zoom in={true} timeout={600}>
             <div
               id="skillCard"
               className="skill-card-animation-wrapper skills-animation"
@@ -75,7 +75,8 @@ const Skills = () => {
               />
             </div>
           </Zoom>
-          <Zoom in={skillCardAnimation} timeout={800}>
+
+          <Zoom in={true} timeout={800}>
             <div
               id="skillCard"
               className="skill-card-animation-wrapper skills-animation"
