78f6e315f5a6729a6d1ac9ccadfc9b525bc71165
Manuel <manuelpinedacabeza@gmail.com>
2023-04-06 18:02:43 +0200

    Imported and added SingleProject components.

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -5,7 +5,7 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Zoom from "@mui/material/Zoom";
 
-import ProjectTile from "../ProjectTile/ProjectTile";
+import SingleProject from "../SingleProject/SingleProject";
 import projects from "../../data/projects";
 import { createObserver, observeElements } from "../../helpers/helpers";
 
@@ -31,11 +31,29 @@ const Projects = () => {
           variant="h3"
           id="projectsHeading"
           className="projects-heading"
-        >
+      >
           My Projects
         </Typography>
 
-        <div id="projectTiles" className="projects-grid projects-animation">
+        <SingleProject
+          projectTitle="Expense Tracker"
+          projectImageUrl="https://user-images.githubusercontent.com/4129325/221216267-0067eb7d-3be7-4771-a621-92907c10105a.png"
+          projectDescription="An Expense Tracker application that can help you track and list expenses, and analyze charts and statistics about them."
+        />
+
+        <SingleProject
+          projectTitle="Motivation Vault"
+          projectImageUrl="https://user-images.githubusercontent.com/4129325/221205796-109b3a7a-a102-46f0-b4f2-3711a883b5fe.png"
+          projectDescription="Motivation Vault is an application that hosts optimistic, inspiring, motivational and educational content. It allows user to create, update read and delete such content, along adding comments and ratings for such content."
+        />
+
+        <SingleProject
+          projectTitle="Media Ratings"
+          projectImageUrl="https://user-images.githubusercontent.com/4129325/221217865-d51a9f01-a1d1-438b-86b0-d85acd3d4351.png"
+          projectDescription="A Media Ratings application where you can search for the IMDb & Rotten Tomatoes ratings for your favorite series."
+        />
+
+        {/* <div id="projectTiles" className="projects-grid projects-animation">
           {projects.map((project, i) => {
             const animTimeout = (i + 1) * 100;
             console.log(animTimeout);
@@ -52,7 +70,7 @@ const Projects = () => {
               </Zoom>
             );
           })}
-        </div>
+        </div> */}
       </Container>
     </Box>
   );
