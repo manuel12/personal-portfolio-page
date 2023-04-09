59d660500729761e585199182c7c4df315abc548
Manuel <manuelpinedacabeza@gmail.com>
2023-04-09 17:55:08 +0200

    Added SingleProjects dynamically.

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -12,11 +12,7 @@ import { createObserver, observeElements } from "../../helpers/helpers";
 import "./styles.css";
 
 const Projects = () => {
-  const [projectTilesAnimation, setProjectTilesAnimation] = useState(false);
-
-  const animationFunctions = {
-    projectTiles: setProjectTilesAnimation,
-  };
+  const animationFunctions = {};
 
   const observer = createObserver(animationFunctions);
 
@@ -35,23 +31,18 @@ const Projects = () => {
           My Projects
         </Typography>
 
-        <SingleProject
-          projectTitle="Expense Tracker"
-          projectImageUrl="https://user-images.githubusercontent.com/4129325/221216267-0067eb7d-3be7-4771-a621-92907c10105a.png"
-          projectDescription="An Expense Tracker application that can help you track and list expenses, and analyze charts and statistics about them."
-        />
-
-        <SingleProject
-          projectTitle="Motivation Vault"
-          projectImageUrl="https://user-images.githubusercontent.com/4129325/221205796-109b3a7a-a102-46f0-b4f2-3711a883b5fe.png"
-          projectDescription="Motivation Vault is an application that hosts optimistic, inspiring, motivational and educational content. It allows user to create, update read and delete such content, along adding comments and ratings for such content."
-        />
-
-        <SingleProject
-          projectTitle="Media Ratings"
-          projectImageUrl="https://user-images.githubusercontent.com/4129325/221217865-d51a9f01-a1d1-438b-86b0-d85acd3d4351.png"
-          projectDescription="A Media Ratings application where you can search for the IMDb & Rotten Tomatoes ratings for your favorite series."
-        />
+        {projects.map((project) => {
+          return (
+            <SingleProject
+              projectName={project.projectName}
+              projectImageUrl={project.projectImgSrc}
+              projectDescription={project.projectName}
+              frontendSkills={project.frontendSkills}
+              backendSkills={project.backendSkills}
+              automatedTestSkills={project.automatedTestSkills}
+            />
+          );
+        })}
       </Container>
     </Box>
   );
