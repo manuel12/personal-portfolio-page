1cd4ef87772c35c8690c1201f843cbcaf6ea0c06
Manuel <manuelpinedacabeza@gmail.com>
2023-04-14 15:23:19 +0200

    Imported react hooks, added more props to component.
    Added grow animatons to typography components.

--- react-portfolio/src/components/SingleProject/SingleProject.jsx
+++ react-portfolio/src/components/SingleProject/SingleProject.jsx
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
+import Grow from "@mui/material/Grow";
 
 import "./styles.css";
 
 const SingleProject = ({
+  projectId,
+  projectAnimationClass,
+  projectAnimation,
   projectName,
   projectImageUrl,
   projectDescription,
   frontendSkills,
   backendSkills,
   automatedTestSkills,
-}) => { 
+}) => {
   return (
-    <div className="single-project-container">
-      <Typography className="project-title" variant="h3" textAlign="left">
-        {projectName}
-      </Typography>
-      <div className="content">
-        <img className="project-image" src={projectImageUrl} alt="" />
+    <div
+      id={projectId}
+      className={`single-project-container ${projectAnimationClass}`}
+    >
+      <Grow in={projectAnimation} timeout={700}>
+        <Typography className='project-title' variant='h3' textAlign='left'>
+          {projectName}
+        </Typography>
+      </Grow>
 
-        <Typography className="project-description" textAlign="left">
-          {projectDescription}
-        </Typography>
-
-        <Typography textAlign="left">
-          Front-end: {frontendSkills && `${frontendSkills.join(", ")}.`}
-        </Typography>
-        <Typography textAlign="left">
-          Back-end: {backendSkills && `${backendSkills.join(", ")}.`}
-        </Typography>
-        <Typography textAlign="left">
-          Automated Tests:{" "}
-          {automatedTestSkills && `${automatedTestSkills.join(", ")}.`}
-        </Typography>
+      <div className='content'>
+        <Grow in={projectAnimation} timeout={800}>
+          <img className='project-image' src={projectImageUrl} alt='' />
+        </Grow>
+        <Grow in={projectAnimation} timeout={900}>
+          <Typography className='project-description' textAlign='left'>
+            {projectDescription}
+          </Typography>
+        </Grow>
+        <Grow in={projectAnimation} timeout={1000}>
+          <Typography textAlign='left'>
+            Front-end: {frontendSkills && `${frontendSkills.join(", ")}.`}
+          </Typography>
+        </Grow>
+        <Grow in={projectAnimation} timeout={1000}>
+          <Typography textAlign='left'>
+            Back-end: {backendSkills && `${backendSkills.join(", ")}.`}
+          </Typography>
+        </Grow>
+        <Grow in={projectAnimation} timeout={1000}>
+          <Typography textAlign='left'>
+            Automated Tests:{" "}
+            {automatedTestSkills && `${automatedTestSkills.join(", ")}.`}
+          </Typography>
+        </Grow>
       </div>
     </div>
   );
