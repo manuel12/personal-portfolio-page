fbc5a79f61aa91eae044d9f6eae32c82e50703dc
Manuel <manuelpinedacabeza@gmail.com>
2023-04-15 17:48:02 +0200

    Formatted code.

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -33,50 +33,50 @@ const Contact = () => {
   });
 
   return (
-    <Box className="contact" id="contact">
-      <Container className="contact-container">
+    <Box className='contact' id='contact'>
+      <Container className='contact-container'>
         <Typography
-          variant="h3"
-          id="contactHeading"
-          className="contact-heading"
+          variant='h3'
+          id='contactHeading'
+          className='contact-heading'
         >
           Contact
         </Typography>
         <Zoom in={contactCardAnimation} timeout={600}>
-          <div id="contactCard" className="contact-card contact-animation">
-            <div className="icon-section">
+          <div id='contactCard' className='contact-card contact-animation'>
+            <div className='icon-section'>
               <Zoom in={socialIconsAnimation} timeout={700}>
                 <a
-                  href="mailto:manuelpinedacabeza.com"
-                  target="_blank"
-                  id="socialIcon"
-                  className="contact-animation"
+                  href='mailto:manuelpinedacabeza.com'
+                  target='_blank'
+                  id='socialIcon'
+                  className='contact-animation'
                 >
-                  <div className="icon-container">
+                  <div className='icon-container'>
                     <EmailIcon />
                   </div>
                 </a>
               </Zoom>
               <Zoom in={socialIconsAnimation} timeout={800}>
                 <a
-                  href="https://www.linkedin.com/"
-                  target="_blank"
-                  id="socialIcon"
-                  className="contact-animation"
+                  href='https://www.linkedin.com/'
+                  target='_blank'
+                  id='socialIcon'
+                  className='contact-animation'
                 >
-                  <div className="icon-container">
+                  <div className='icon-container'>
                     <LinkedInIcon />
                   </div>
                 </a>
               </Zoom>
               <Zoom in={socialIconsAnimation} timeout={900}>
                 <a
-                  href="https://github.com/manuel12"
-                  target="_blank"
-                  id="socialIcon"
-                  className="contact-animation"
+                  href='https://github.com/manuel12'
+                  target='_blank'
+                  id='socialIcon'
+                  className='contact-animation'
                 >
-                  <div className="icon-container">
+                  <div className='icon-container'>
                     <GitHubIcon />
                   </div>
                 </a>
@@ -85,44 +85,44 @@ const Contact = () => {
 
             <Zoom in={contactQuestionAnimation} timeout={600}>
               <Typography
-                variant="body1"
-                id="contactQuestion"
-                className="contact-animation"
+                variant='body1'
+                id='contactQuestion'
+                className='contact-animation'
               >
                 Do you want to work together?
               </Typography>
             </Zoom>
 
             <Box
-              component="form"
-              id="contactForm"
-              className="contact-animation"
+              component='form'
+              id='contactForm'
+              className='contact-animation'
             >
               <Zoom in={contactFormAnimation} timeout={700}>
                 <input
-                  id="name"
-                  type="text"
-                  name="name"
-                  placeholder="Your name..."
+                  id='name'
+                  type='text'
+                  name='name'
+                  placeholder='Your name...'
                 />
               </Zoom>
               <Zoom in={contactFormAnimation} timeout={800}>
                 <input
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="Your email..."
+                  id='email'
+                  type='email'
+                  name='email'
+                  placeholder='Your email...'
                 />
               </Zoom>
               <Zoom in={contactFormAnimation} timeout={900}>
                 <textarea
-                  id="message"
-                  name="message"
-                  placeholder="Your message..."
+                  id='message'
+                  name='message'
+                  placeholder='Your message...'
                 ></textarea>
               </Zoom>
               <Zoom in={contactFormAnimation} timeout={1000}>
-                <button type="submit" className="submit-btn">
+                <button type='submit' className='submit-btn'>
                   Submit
                 </button>
               </Zoom>
