95503adfa616c27de4e0a2bb7a7af021de0a3f1c
Manuel <manuelpinedacabeza@gmail.com>
2023-05-10 08:07:53 +0200

    Imported and used Certifications component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+import Certifications from "./components/Certifications/Certifications";
 
 function App() {
   const handleScroll = () => {
@@ -22,6 +23,7 @@ function App() {
       <Navbar />
       <Home />
       <Skills />
+      <Certifications />
       <Projects />
       <Contact />
     </div>
