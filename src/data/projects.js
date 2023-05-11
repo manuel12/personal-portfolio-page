ebe9bedb863f927b50c1b240cedb2df71a47b979
Manuel <manuelpinedacabeza@gmail.com>
2023-05-11 15:46:07 +0200

    Added technologies data

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,44 +2,74 @@ const projects = [
   {
     projectName: "Expense Tracker",
     projectUrl: "https://github.com/manuel12/django-expense-tracker-public",
+    projectTechnologies: [
+      "html5",
+      "css3",
+      "bootstrap",
+      "javascript",
+      "cypress",
+      "python",
+      "django",
+    ],
     projectImgSrc:
       "https://user-images.githubusercontent.com/4129325/221216267-0067eb7d-3be7-4771-a621-92907c10105a.png",
     projectDesc:
       "An Expense Tracker application that can help you track and list expenses, and analyze charts and statistics about them.",
-    frontendSkills: ["HMTL", "CSS", "JS", "Bootstrap"],
-    backendSkills: ["Python", "Django"],
-    automatedTestSkills: ["JS", "Cypress", "Cypress Image Snapshot"],
+    frontend: true,
+    backend: true,
+    automatedTest: true,
   },
   {
     projectName: "Motivation Vault",
     projectUrl: "https://github.com/manuel12/motivation-vault",
+    projectTechnologies: [
+      "html5",
+      "css3",
+      "javascript",
+      "react",
+      "cypress",
+      "python",
+      "django",
+    ],
     projectImgSrc:
       "https://user-images.githubusercontent.com/4129325/221205796-109b3a7a-a102-46f0-b4f2-3711a883b5fe.png",
     projectDesc:
       "Motivation Vault is an application that hosts optimistic, inspiring, motivational and educational content. It allows user to create, update read and delete such content, along adding comments and ratings for such content.",
-    frontendSkills: ["HMTL", "CSS", "JS", "React"],
-    backendSkills: ["Python", "Django", "DRF"],
-    automatedTestSkills: ["JS", "Cypress", "Cypress Image Snapshot"],
+    frontend: true,
+    backend: true,
+    automatedTest: true,
   },
   {
     projectName: "Media Ratings",
     projectUrl: "https://github.com/manuel12/media-ratings",
+    projectTechnologies: [
+      "html5",
+      "css3",
+      "bootstrap",
+      "javascript",
+      "react",
+      "cypress",
+      "python",
+      "django",
+      ,
+    ],
     projectImgSrc:
       "https://user-images.githubusercontent.com/4129325/221217865-d51a9f01-a1d1-438b-86b0-d85acd3d4351.png",
     projectDesc:
       "A Media Ratings application where you can search for the IMDb & Rotten Tomatoes ratings for your favorite series.",
-    frontendSkills: ["HMTL", "CSS", "JS", "Bootstrap", "React"],
-    backendSkills: ["Python", "Django", "DRF"],
-    automatedTestSkills: ["JS", "Cypress", "Cypress Image Snapshot"],
+    frontend: true,
+    backend: true,
+    automatedTest: true,
   },
   {
     projectName: "MUI Landing Page",
     projectUrl: "https://github.com/manuel12/mui-landing-page",
+    projectTechnologies: ["html5", "css3", "javascript", "react", "materialui"],
     projectImgSrc:
       "https://user-images.githubusercontent.com/4129325/230984164-8bcdccba-f995-4d89-b308-a6a0f59b32ea.png",
     projectDesc:
       "A landing page for an italian pizzeria made with Material UI.",
-    frontendSkills: ["HMTL", "CSS", "JS", "React", "Material UI"],
+    frontend: true,
   },
 
   // {
---
+++ src/data/technologies.js
@@ -0,0 +1,32 @@
+const technologies = {
+  figma:
+    "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/figma/figma-original.svg",
+  html5:
+    "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-original.svg",
+  css3: "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-original.svg",
+
+  bootstrap:
+    "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/bootstrap/bootstrap-original.svg",
+  javascript:
+    "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/javascript/javascript-original.svg",
+  react:
+    "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original.svg",
+
+  materialui:
+    "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/materialui/materialui-original.svg",
+  cypress:
+    "https://user-images.githubusercontent.com/4129325/221191364-90ec4583-f906-43f6-969b-bac956846fbd.svg",
+  python:
+    "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/python/python-original.svg",
+
+  django:
+    "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/django/django-plain.svg",
+  git: "https://github.com/devicons/devicon/blob/master/icons/git/git-plain.svg",
+  github:
+    "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/git/git-plain.svg",
+
+  heroku:
+    "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/heroku/heroku-plain.svg",
+};
+
+export default technologies;
