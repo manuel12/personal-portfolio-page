2a53cb1feecc966c4279b5f1a6195628fb010f20
Manuel <manuelpinedacabeza@gmail.com>
2023-05-12 17:04:18 +0200

    Added projectRepoUrl, projectLiveDemoUrl, projectMobileImgSrc to project objects.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,8 @@
 const projects = [
   {
     projectName: "Expense Tracker",
-    projectUrl: "https://github.com/manuel12/django-expense-tracker-public",
+    projectRepoUrl: "https://github.com/manuel12/django-expense-tracker-public",
+    projectLiveDemoUrl: false,
     projectTechnologies: [
       "html5",
       "css3",
@@ -13,6 +14,8 @@ const projects = [
     ],
     projectImgSrc:
       "https://user-images.githubusercontent.com/4129325/221216267-0067eb7d-3be7-4771-a621-92907c10105a.png",
+    projectMobileImgSrc:
+      "https://user-images.githubusercontent.com/4129325/221216763-897b1481-2626-4b24-ab2c-ce424d24a51f.png",
     projectDesc:
       "An Expense Tracker application that can help you track and list expenses, and analyze charts and statistics about them.",
     frontend: true,
@@ -21,7 +24,8 @@ const projects = [
   },
   {
     projectName: "Motivation Vault",
-    projectUrl: "https://github.com/manuel12/motivation-vault",
+    projectRepoUrl: "https://github.com/manuel12/motivation-vault",
+    projectLiveDemoUrl: false,
     projectTechnologies: [
       "html5",
       "css3",
@@ -33,6 +37,8 @@ const projects = [
     ],
     projectImgSrc:
       "https://user-images.githubusercontent.com/4129325/221205796-109b3a7a-a102-46f0-b4f2-3711a883b5fe.png",
+    projectMobileImgSrc:
+      "https://user-images.githubusercontent.com/4129325/221207023-de613790-db09-4513-ae9e-ba893a575d54.png",
     projectDesc:
       "Motivation Vault is an application that hosts optimistic, inspiring, motivational and educational content. It allows user to create, update read and delete such content, along adding comments and ratings for such content.",
     frontend: true,
@@ -41,7 +47,8 @@ const projects = [
   },
   {
     projectName: "Media Ratings",
-    projectUrl: "https://github.com/manuel12/media-ratings",
+    projectRepoUrl: "https://github.com/manuel12/media-ratings",
+    projectLiveDemoUrl: false,
     projectTechnologies: [
       "html5",
       "css3",
@@ -55,6 +62,8 @@ const projects = [
     ],
     projectImgSrc:
       "https://user-images.githubusercontent.com/4129325/221217865-d51a9f01-a1d1-438b-86b0-d85acd3d4351.png",
+    projectMobileImgSrc:
+      "https://user-images.githubusercontent.com/4129325/238203734-7074b91e-8a14-47e6-9307-51330714b22b.png",
     projectDesc:
       "A Media Ratings application where you can search for the IMDb & Rotten Tomatoes ratings for your favorite series.",
     frontend: true,
@@ -63,46 +72,35 @@ const projects = [
   },
   {
     projectName: "MUI Landing Page",
-    projectUrl: "https://github.com/manuel12/mui-landing-page",
+    projectRepoUrl: "https://github.com/manuel12/mui-landing-page",
+    projectLiveDemoUrl: "https://manuel12.github.io/mui-landing-page/",
     projectTechnologies: ["html5", "css3", "javascript", "react", "materialui"],
     projectImgSrc:
       "https://user-images.githubusercontent.com/4129325/230984164-8bcdccba-f995-4d89-b308-a6a0f59b32ea.png",
+    projectMobileImgSrc:
+      "https://user-images.githubusercontent.com/4129325/230983671-033238e0-22b7-4436-ae56-c7c34d0e3e43.png",
     projectDesc:
       "A landing page for an italian pizzeria made with Material UI.",
     frontend: true,
   },
 
   // {
-  //   projectUrl: "https://github.com/manuel12/custom-css-library",
+  //   projectRepoUrl: "https://github.com/manuel12/custom-css-library",
   //   projectName: "Custom Library",
   //   projectImgSrc: "images/custom-library.png",
   // },
   // {
-  //   projectUrl: "https://github.com/manuel12/technical-documentation-page",
+  //   projectRepoUrl: "https://github.com/manuel12/technical-documentation-page",
   //   projectName: "Custom Library - Documentation",
   //   projectImgSrc: "images/technical-documentation-page.png",
   // },
   // {
-  //   projectUrl: "https://github.com/manuel12/landing-page",
+  //   projectRepoUrl: "https://github.com/manuel12/landing-page",
   //   projectName: "Landing Page",
   //   projectImgSrc: "images/landing-page.png",
   // },
 
   // {
-  //   projectUrl: "https://github.com/manuel12/quote-project",
-  //   projectName: "Random Quote Generator",
-  //   projectImgSrc: "images/random-quote-generator.png",
-  // },
-  // {
-  //   projectUrl: "https://github.com/manuel12/25-plus-5-clock",
-  //   projectName: "25 + 5 Clock",
-  //   projectImgSrc: "images/25_plus_5_clock.png",
-  // },
-  // {
-  //   projectUrl: "https://github.com/manuel12/js-calculator",
-  //   projectName: "JS Calculator",
-  //   projectImgSrc: "images/js-calculator.png",
-  // },
 ];
 
 export default projects;
